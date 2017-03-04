6f39501bdf2c1587bcda2bb5ca614654facd9222
Haukur Kristinsson <haukur@hauxi.is>
2017-03-04 13:34:44 +0000

    Add batch file to load java packages on Windows

---
+++ load-into-oracle.bat
@@ -0,0 +1,13 @@
+set USER_PASS='sce/XXX'
+set SCHEMA='SCE'
+set LOADJAVA_OPTS="-v -u %USER_PASS%"
+
+mvn clean package
+mvn dependency:copy-dependencies
+
+# load dependencies
+loadjava %LOADJAVA_OPTS% -resolve -resolver "((* %SCHEMA%) (* PUBLIC) (* -))" target/dependency/commons-logging*.jar
+loadjava %LOADJAVA_OPTS% -resolve -resolver "((* %SCHEMA%) (* PUBLIC) (* -))" target/dependency/amqp-client*.jar
+
+# load the actual Java procedures
+loadjava %LOADJAVA_OPTS% -resolve -resolver "((* %SCHEMA%) (* PUBLIC) (* -))" target/classes/com/zenika/oracle/amqp/*.class
